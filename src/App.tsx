bc9e683c49ff00ac1e0d509735feb33df7717007
Jack Dean <jack@jacks-air.lan>
2021-08-21 17:21:25 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, Finn!
         </a>
       </header>
     </div>
