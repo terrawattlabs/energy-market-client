8cf0f6a0e958843c05751888013fb2f284702d69
Jack Dean <jack@jacks-air.lan>
2021-10-29 15:12:51 -0400

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -18,27 +18,6 @@ const App: React.FC<Props> = ({ basename }) => {
     shallowEqual
   );
 
-  const localAvail = async () =>{
-    fetch('http://192.168.86.160:4000/energio/status', {method: 'GET'})
-    .then((r)=>{
-      // yep the local is available, redirect the user to the locally hosted version of the app
-      window.location.href = 'http://192.168.86.160:4000'; 
-    })
-    .catch((err)=>{
-      // got an error from the fetch, meaning the local is not available. Keep the user here and log the error just for dev
-        console.log(err)
-    })
-  };
-
-  //check if the user is already on the locally running instance (just seeing if the current window href includes the specific IP)
-  if(window.location.href.indexOf('192.168.86.160') > -1) {
-    console.log('on the local!')
-    // if already on the local, just do nothing
-  } else {
-    // not already  on the local? no problem check to see if the local is available for usre
-    localAvail();
-  }
-  
 
   return (
     <BrowserRouter basename={basename}>
